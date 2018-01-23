8b2f1c3ee0573576c5a1e3a0a288fadd2a2f3c96
hbar4 <35719981+hbar4@users.noreply.github.com>
2018-01-23 10:57:15 -0800

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,29 @@
+var steps = 0
+while (steps < 8) {
+  right
+  steps = steps + 1
+  if (GetColor!= “red” ) {
+  SetColor ("red")
+} 
+}
+while (steps < 4) {
+  down
+  steps = steps + 1
+  if (GetColor!= “red” ) {
+  SetColor ("red")
+} 
+}
+while (steps < 8) {
+  left
+  steps = steps + 1
+  if (GetColor!= “red” ) {
+  SetColor ("red")
+} 
+}
+while (steps < 8) {
+  up
+  steps = steps + 1
+  if (GetColor!= “red” ) {
+  SetColor ("red")
+} 
+}
