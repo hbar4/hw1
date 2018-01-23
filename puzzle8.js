3d4dfdc6e11ba57efcc3207c07535d7a121e4e36
hbar4 <35719981+hbar4@users.noreply.github.com>
2018-01-23 11:10:45 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,28 @@
+down
+function downloop {
+down
+down
+down
+down
+down
+down
+down
+right
+right
+}
+function uploop {
+up
+up
+up
+up
+up
+up
+up
+left
+left
+}
+downloop
+uploop
+downloop
+uploop
+downloop
